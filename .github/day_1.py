9c1ff24f49750a9d5bbde571424898cda66912ef
Rimwaka'sP <94560702+rimwaka@users.noreply.github.com>
2025-01-07 10:59:55 +0300

    day_1_of_the_challenge

---
+++ .github/day_1.py
@@ -0,0 +1,13 @@
+# Printing strings in Python
+print("Hello World")
+
+# Performing algebraic calculations in Python
+# Example 1
+print((2**8)+(8/2.5)
+
+# Example 2
+a = 3
+b = 8
+c = 10
+
+print(3*8**10)
