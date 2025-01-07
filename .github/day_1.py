058a868fc551111c06e33c1c7c1996ff9a4010f7
Rimwaka'sP <94560702+rimwaka@users.noreply.github.com>
2025-01-07 11:05:06 +0300

    day_1_of_the_challenge

--- .github/day_1.py
+++ .github/day_1.py
@@ -1,13 +1,15 @@
 # Printing strings in Python
-print("Hello World")
+print("Hello World!")
+# Output: Hello World!
 
 # Performing algebraic calculations in Python
 # Example 1
-print((2**8)+(8/2.5)
-
+print((2**8)/(8/2.5))
+#Output: 80.0
 # Example 2
 a = 3
 b = 8
 c = 10
+print(a*b**c)
+#Output:3221225472
 
-print(3*8**10)
